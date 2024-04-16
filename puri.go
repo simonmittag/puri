2a963b65a3e3649ad38f5324bf703d327c85a8d6
Simon Mittag <simonmittag@gmail.com>
2024-04-17 06:23:58 +1000

    tests

--- puri.go
+++ puri.go
@@ -2,11 +2,11 @@ package puri
 
 import "net/url"
 
-const Version string = "v0.1"
+const Version string = "v0.1.2"
 
 func ExtractParam(uri string, param string) (string, error) {
 	parsed, err := url.Parse(uri)
-	if err != nil {
+	if err != nil || len(uri) == 0 {
 		return "", err
 	}
 	v := parsed.Query()

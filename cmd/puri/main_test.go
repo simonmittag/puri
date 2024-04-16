538b145b3b4968259b645539eec02a7437cb245e
Simon Mittag <simonmittag@gmail.com>
2024-04-17 06:21:46 +1000

    main tests

--- cmd/puri/main_test.go
+++ cmd/puri/main_test.go
@@ -11,13 +11,16 @@ func TestParseFlags(t *testing.T) {
 		t.Error(err)
 	}
 }
+
 func TestParseFlagSet(t *testing.T) {
 	flagset := flag.NewFlagSet("test", flag.ExitOnError)
-	err := ParseFlagSet(flagset, []string{"-p", "test"})
+	flagset.String("p", "", "extract uri param")
+	err := flagset.Parse([]string{"-p", "test"})
 	if err != nil {
 		t.Error(err)
 	}
 }
+
 func TestParseArgs(t *testing.T) {
 	valid := []string{"http://localhost"}
 	_, err := parseArgs(valid)
